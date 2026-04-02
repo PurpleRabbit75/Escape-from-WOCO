88c22c9368a522a8784393ad0992b081d1df08c7
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-04-02 18:46:40 -0500

    added pin

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ Sam = Group(
     Circle(50, 30, 10, fill = "black")
 )
 
+pin = Group(
+    Rect(107.5, 100, 5, 50, fill = gradient("gold", "yellow")),
+    Circle(110, 90, 10, fill = None, border = gradient("gold", "yellow"), borderWidth = 5),
+    Rect(110-10, 100, 20, 5, fill = gradient("gold", "yellow"))
+)
+
 def onKeyPress(key):
     if key == "up":
         Sam.centerY -= 10
