2c990a9a3b1c7a9b2500c8624427276c7f4ee49a
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-04-02 18:49:47 -0500

    added "Sam" collision demo and fall mechanics

--- main.py
+++ main.py
@@ -24,6 +24,21 @@ def onKeyPress(key):
     elif key == "right":
         Sam.centerX += 10
 
+g = 0.2
+vSam_0 = 0
+
+def onStep():
+    global vSam_0
+    if Sam.hitsShape(pin):
+        pin.fill = "red"
+    else:
+        pin.fill = gradient("gold", "yellow")
+    
+    Sam.centerY += vSam_0
+    vSam_0 += g
+    
+
+
 
 
 cmu_graphics.run()
