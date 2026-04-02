cc984754e1bbd7bcac1f0b2aa075663673a2fb1f
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-04-02 18:37:41 -0500

    added movers for sam

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ Sam = Group(
     Circle(50, 30, 10, fill = "black")
 )
 
+def onKeyPress(key):
+    if key == "up":
+        Sam.centerY -= 10
+    elif key == "down":
+        Sam.centerY += 10
+    elif key == "left":
+        Sam.centerX -= 10
+    elif key == "right":
+        Sam.centerX += 10
+
+
 
 cmu_graphics.run()
