f57dceb2b6b8811580cf8ccb57d0312f95c18f1e
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-04-02 18:35:55 -0500

    added Sam

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ from cmu_graphics import *
 Rect(0, 0, 20, 20, fill = "blue")
 
 
+Sam = Group(
+    Rect(40, 40, 20, 40, fill = "black"),
+    Circle(50, 30, 10, fill = "black")
+)
+
+
 cmu_graphics.run()
