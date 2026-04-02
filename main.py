66217f00aeebd0e56c36b1abbe6fa4fa9a791c38
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-04-02 18:52:54 -0500

    added objects file

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from cmu_graphics import *
+from objects import *
 
-Rect(0, 0, 20, 20, fill = "blue")
 
-
-Sam = Group(
-    Rect(40, 40, 20, 40, fill = "black"),
-    Circle(50, 30, 10, fill = "black")
-)
-
-pin = Group(
-    Rect(107.5, 100, 5, 50, fill = gradient("gold", "yellow")),
-    Circle(110, 90, 10, fill = None, border = gradient("gold", "yellow"), borderWidth = 5),
-    Rect(110-10, 100, 20, 5, fill = gradient("gold", "yellow"))
-)
 
 def onKeyPress(key):
     if key == "up":
