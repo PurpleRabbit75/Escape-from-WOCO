0feba2911dbff39ab96a3eac267fd05e55e617cd
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-04-02 19:38:24 -0500

    added imports to main.py

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 from cmu_graphics import *
 from sprites import *
-from fluid_sim import Lava, Water, fluid_step, bucket
+from fluid_sim import Lava, Water, fluid_step
+if True:
+    import classes.ball
+    import classes.collidable
+    import classes.lava
+    import classes.pin
+    import classes.spawner
+    import classes.wall
+    import classes.water
+    import classes.bucket
 
 
 
-def onKeyPress(key):
-    if key == "up":
-        Sam.centerY -= 10
-    elif key == "down":
-        Sam.centerY += 10
-    elif key == "left":
-        Sam.centerX -= 10
-    elif key == "right":
-        Sam.centerX += 10
+# def onKeyPress(key):
+#     if key == "up":
+#         Sam.centerY -= 10
+#     elif key == "down":
+#         Sam.centerY += 10
+#     elif key == "left":
+#         Sam.centerX -= 10
+#     elif key == "right":
+#         Sam.centerX += 10
 
-g = 0.2
-vSam_0 = 0
+# g = 0.2
+# vSam_0 = 0
 
-def onStep():
+# def onStep():
 
-    fluid_step()
+#     fluid_step()
 
-    global vSam_0
-    if not Sam.hitsShape(bucket):
-        Sam.centerY += vSam_0
-        vSam_0 += g
-    else:
-        vSam_0 = 0
+#     global vSam_0
+#     if not Sam.hitsShape(bucket):
+#         Sam.centerY += vSam_0
+#         vSam_0 += g
+#     else:
+#         vSam_0 = 0
   
 
     
--- set.py
+++
@@ -1,6 +0,0 @@
-from cmu_graphics import *
-
-bucket = Group(
-    Rect(50, 200, 100, 20, fill = "blue"),
-    Rect(200, 300, 20, 100, fill = "blue", rotateAngle = 45)
-)
